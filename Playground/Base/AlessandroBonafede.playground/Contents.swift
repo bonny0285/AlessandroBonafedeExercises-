39e65efa5a5eebe0c77413bdd46eeeef61613adf
Massimiliano Bonafede <massimiliano.bonafede@altran.it>
2021-06-22 14:37:49 +0200

    added three new exercises

--- Playground/Base/AlessandroBonafede.playground/Contents.swift
+++ Playground/Base/AlessandroBonafede.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ Fare il confronto tra due variabili o costanti usando gli operatori di confronto <, >, <=, >=, =
+ Esempio:
+ let giovanni: Int = 10
+ let maria: Int = 9
+ let result = giovanni > maria
+ */
+
+
+/*
+ Fare il confronto tra due variabili o costanti usando gli operatori +, -, *, /
+ Esempio:
+ let giovanni: Int = 10
+ let maria: Int = 9
+ let result = giovanni + maria
+ */
+
+/*
+ Usando un IF ELSE fare il print dei vari stati di una variabile
+ Esempio:
+ let marco: Int = 10
+ let mario: Int = 5
+
+ if marco == mario {
+     print("Mario e Marco sono uguali")
+ } else if marco > mario {
+     print("Marco è maggiore di Mario")
+ } .........
+ */
+
+
